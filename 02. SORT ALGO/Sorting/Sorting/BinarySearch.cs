e3fc1efea8601daae72da545f754143a9fab2a92
DKondoff <danielkondov91gmail.com>
2019-03-13 18:23:47 +0200

    BinarySearch

---
+++ 02. SORT ALGO/Sorting/Sorting/BinarySearch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    public class BinarySearch
+    {
+        public static int IndexOf(int[] arr, int key)
+        {
+            int min = 0;
+            int max = arr.Length - 1;
+
+            while (min <= max)
+            {
+                int mid = (max + min) / 2;
+
+                if (key == arr[mid])
+                {
+                    return mid;
+                }
+                else if (key < arr[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int IndexOf(int[] arr, int key, int min, int max)
+        {
+            if (min > max)
+            {
+                return -1;
+            }
+            else
+            {
+                int mid = (min + max) / 2;
+
+                if (key == arr[mid])
+                {
+                    return mid;
+                }
+                else if(key < arr[mid])
+                {
+                    return IndexOf(arr, key, min, mid - 1);
+                }
+                else
+                {
+                    return IndexOf(arr, key, mid + 1, max);
+                }
+            }
+        }
+    }
+}
--- 02. SORT ALGO/Sorting/Sorting/Program.cs
+++ 02. SORT ALGO/Sorting/Sorting/Program.cs
@@ -8,10 +8,16 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            Quick.Sort(new int[] { 10, 7, 8, 9, 1, 5 });
+            //Quick.Sort(new int[] { 10, 7, 8, 9, 1, 5 });
+            //Quick.Sort(new int[] { 1, 2, 3, 6, 5, 4 });
 
-            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            QuickSortGen.Sort(arr);
+            //var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            //QuickSortGen.Sort(arr);
+
+            Console.WriteLine(BinarySearch.IndexOf(new int[] { -1, 0, 1, 2, 4 }, 2));
+            Console.WriteLine(BinarySearch.IndexOf(new int[] { 1,2,3,4,5 }, 1));
+            Console.WriteLine(BinarySearch.IndexOf(new int[] { -1, 0, 1, 2, 4 }, 2, 0, 5));
+            Console.WriteLine(BinarySearch.IndexOf(new int[] { 1, 2, 3, 4, 5 }, 1, 0, 5));
         }
     }
 }
