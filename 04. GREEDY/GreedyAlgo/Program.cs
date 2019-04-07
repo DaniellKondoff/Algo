b89695979fdbcbcf02f8debb91a0f4752b556566
Daniel Kondov <danielkondov91@gmail.com>
2019-04-07 12:51:57 +0300

    Greedy

---
+++ 04. GREEDY/GreedyAlgo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreedyAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
