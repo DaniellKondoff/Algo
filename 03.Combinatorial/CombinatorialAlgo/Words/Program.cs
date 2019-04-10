7f2506d0b873e4944e76e07dec217ece356485d4
Daniel Kondov <danielkondov91@gmail.com>
2019-04-10 09:37:44 +0300

    Words

---
+++ 03.Combinatorial/CombinatorialAlgo/Words/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Words
+{
+    class Program
+    {
+        public static char[] elements;
+        private static int count;
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            elements = input.ToCharArray();
+
+            var allUnique = input.Distinct().Count() == input.Length;
+
+            if (allUnique)
+            {
+                count = 1;
+
+                for (int i = 1; i <= input.Length; i++)
+                {
+                    count *= i;
+                }
+            }
+            else
+            {
+                Permute(0);
+            }
+
+            Console.WriteLine(count);
+        }
+
+        public static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                for (int i = 1; i < elements.Length; i++)
+                {
+                    if (elements[i] == elements[i - 1])
+                    {
+                        return;
+                    }
+                }
+
+                count++;
+            }
+            else
+            {
+                HashSet<int> swapped = new HashSet<int>();
+
+                for (int i = index; i < elements.Length; i++)
+                {
+                    if (!swapped.Contains(elements[i]))
+                    {
+                        Swap(index, i);
+                        Permute(index + 1);
+                        Swap(index, i);
+                        swapped.Add(elements[i]);
+                    }
+                }
+
+            }
+        }
+
+        private static void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = elements[firstIndex];
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = temp;
+        }
+    }
+}
