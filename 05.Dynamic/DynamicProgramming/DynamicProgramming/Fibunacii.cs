05b2983babf5c2cda6dc9eaf36b95dcc669e54ae
Daniel Kondov <danielkondov91@gmail.com>
2019-04-15 22:20:41 +0300

    DP

---
+++ 05.Dynamic/DynamicProgramming/DynamicProgramming/Fibunacii.cs
@@ -0,0 +1,32 @@
+﻿namespace DynamicProgramming
+{
+    public class Fibunacii
+    {
+        public static long[] numbers;
+
+        public Fibunacii(int fibNum)
+        {
+            numbers = new long[fibNum + 1];
+
+        }
+
+        public long Fib(int n)
+        {
+            if (numbers[n] != 0)
+            {
+                return numbers[n];
+            }
+
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            var result = Fib(n - 1) + Fib(n - 2);
+
+            numbers[n] = result;
+
+            return result;
+        }
+    }
+}
---
+++ 05.Dynamic/DynamicProgramming/DynamicProgramming/LongestIncreasingSubsequence.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DynamicProgramming
+{
+    public class LongestIncreasingSubsequence
+    {
+        public static void Calc()
+        {
+            var numbers = new int[] { 3, 14, 5, 12, 15, 7, 8, 9, 11, 10, 1 };
+
+            var solutions = new int[numbers.Length];
+            var prev = new int[numbers.Length];
+
+            var maxSolution = 0;
+            var maxSolutionIndex = 0;
+
+            for (int current = 0; current < numbers.Length; current++)
+            {
+                var solution = 1;
+                var prevIndex = -1;
+                var currentNumber = numbers[current];
+
+                for (int solIndex = 0; solIndex < current; solIndex++)
+                {
+                    var prevNumber = numbers[solIndex];
+                    var prevSolution = solutions[solIndex];
+
+                    if (currentNumber > prevNumber && solution <= prevSolution)
+                    {
+                        solution = prevSolution + 1;
+                        prevIndex = solIndex;
+                    }
+                }
+
+                solutions[current] = solution;
+                prev[current] = prevIndex;
+
+                if (solution > maxSolution)
+                {
+                    maxSolution = solution;
+                    maxSolutionIndex = current;
+                }
+            }
+
+            Console.WriteLine(maxSolution);
+
+            var index = maxSolutionIndex;
+            var result = new List<int>();
+
+            while (index != -1)
+            {
+                var currentNumber = numbers[index];
+                result.Add(currentNumber);
+                index = prev[index];
+            }
+
+            result.Reverse();
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
